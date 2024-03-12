3a80e39a03143c643e291e54546fdc347e8342e7
Arseny Radoslavov <rad.archeg@gmail.com>
2024-03-12 20:13:35 +0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import WorldMap from '@/components/WorldMap';
+
+export default function Home() {
+
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <WorldMap className="stroke-blue-300"/>
+    </div>
+  );
+}
